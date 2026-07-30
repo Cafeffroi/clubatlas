e7c44a2fe094f56de2d38c095b172eaa516aa2c1
Ringler Francois (LEC-Viveris) <Francois.Ringler.Viveris@liebherr.com>
2026-07-30 16:34:19 +0200

    better club page

--- src/app/club/contact/contact.component.ts
+++ src/app/club/contact/contact.component.ts
@@ -1,24 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {
   FormBuilder,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { SafePipe } from '../../../utils/safe.pipe';
+import { GoogleMapsModule } from '@angular/google-maps';
 import { Club } from '../../models/club.model';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-contact',
-  imports: [ReactiveFormsModule, SafePipe],
+  imports: [CommonModule, ReactiveFormsModule, GoogleMapsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
-export class ContactComponent {
-  contactForm: FormGroup;
-  mapUrl = 'https://www.google.com/maps/embed?pb=YOUR_EMBED_URL';
-
+export class ContactComponent implements OnInit {
   @Input({ required: true }) club!: Club;
+
+  contactForm: FormGroup;
+  mapOptions!: google.maps.MapOptions;
 
   constructor(private fb: FormBuilder) {
     this.contactForm = this.fb.group({
@@ -28,10 +30,32 @@ export class ContactComponent {
     });
   }
 
+  ngOnInit(): void {
+    this.mapOptions = {
+      center: this.club.position,
+      zoom: 15,
+      disableDefaultUI: true,
+      zoomControl: true,
+      mapId: environment.googleMaps.mapId,
+    };
+  }
+
+  onMapInitialized(map: google.maps.Map): void {
+    new google.maps.marker.AdvancedMarkerElement({
+      map,
+      position: this.club.position,
+      title: this.club.name,
+    });
+  }
+
+  get directionsUrl(): string {
+    const { lat, lng } = this.club.position;
+    return `https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}`;
+  }
+
   onSubmit() {
     if (this.contactForm.valid) {
       console.log(this.contactForm.value);
-      // Add your form submission logic here
     }
   }
 }
